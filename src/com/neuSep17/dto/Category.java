71cb9d9be1ace27f43890e366120332d090ce0cf
Yiteng <31704724+GeekyNerds@users.noreply.github.com>
2017-11-18 17:14:16 -0800

    add getCategory() methods to convert String type of category in the file to enum Category type

--- src/com/neuSep17/dto/Category.java
+++ src/com/neuSep17/dto/Category.java
@@ -2,5 +2,25 @@ package com.neuSep17.dto;
 
 public enum Category {
 
-    NEW, USED, CERTIFIED
+    NEW, USED, CERTIFIED;
+        
+        
+    public static Category getCategory(String cat){
+        switch (cat){
+           case "used": return USED;
+           case "new": return NEW;
+           case "certified": return CERTIFIED;
+           default: throw new IllegalArgumentException();
+       }
+    }
+
+    @Override
+    public String toString() {
+        switch (this){
+           case NEW: return "NEW";
+           case USED: return "USED";
+           case CERTIFIED: return "CERTIFIED";
+           default: throw new IllegalArgumentException();
+        }
+    }
 }
