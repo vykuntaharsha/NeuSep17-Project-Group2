cbf7a8d8cb13c68679994fb17251ef389f1df701
YujiaNEU2017 <zhang.yujia@husky.neu.edu>
2017-11-20 09:57:59 -0800

    update "import" and "package"

--- src/com/neuSep17/dto/Vehicle.java
+++ src/com/neuSep17/dto/Vehicle.java
@@ -1,8 +1,10 @@
 package com.neuSep17.dto;
 
+import java.net.MalformedURLException;
 import java.net.URL;
 
-public class Vehicle {
+public class Vehicle
+{
 
     private String id;
     private String webId;
@@ -14,8 +16,9 @@ public class Vehicle {
     private String bodyType;
     private Float price;
     private URL photoUrl;
-    
-    Vehicle(String[] arr) {
+
+    public Vehicle(String[] arr)
+    {
         this.id = arr[0];
         this.webId = arr[1];
         this.category = Category.getCategory(arr[2].toLowerCase());
@@ -25,63 +28,72 @@ public class Vehicle {
         this.trim = arr[6];
         this.bodyType = arr[7];
         this.price = Float.parseFloat(arr[8]);
-        try {
+        try
+        {
             this.photoUrl = new URL(arr[9]);
-        } catch (MalformedURLException e) {
+        }
+        catch (MalformedURLException e)
+        {
             e.printStackTrace();
         }
     }
-    
+
     @Override
-    public String toString() {
-    		String str= this.id+"~"+this.webId+"~"+this.category+"~"+this.year+"~"+this.make
-    				+"~"+this.model+"~"+this.trim+"~"+this.bodyType+"~"+this.price+"~"+this.photoUrl;
-    		return str;
+    public String toString()
+    {
+        String str = this.id + "~" + this.webId + "~" + this.category + "~" + this.year + "~" + this.make + "~"
+                + this.model + "~" + this.trim + "~" + this.bodyType + "~" + this.price + "~" + this.photoUrl;
+        return str;
     }
 
-	public String getId() {
-		return id;
-	}
+    public String getId()
+    {
+        return id;
+    }
 
-	public String getWebId() {
-		return webId;
-	}
+    public String getWebId()
+    {
+        return webId;
+    }
 
-	public Integer getYear() {
-		return year;
-	}
+    public Integer getYear()
+    {
+        return year;
+    }
 
-	public String getMake() {
-		return make;
-	}
+    public String getMake()
+    {
+        return make;
+    }
 
-	public String getModel() {
-		return model;
-	}
+    public String getModel()
+    {
+        return model;
+    }
 
-	public String getTrim() {
-		return trim;
-	}
+    public String getTrim()
+    {
+        return trim;
+    }
 
-	public Category getCategory() {
-		return category;
-	}
+    public Category getCategory()
+    {
+        return category;
+    }
 
-	public String getBodyType() {
-		return bodyType;
-	}
+    public String getBodyType()
+    {
+        return bodyType;
+    }
 
-	public Float getPrice() {
-		return price;
-	}
+    public Float getPrice()
+    {
+        return price;
+    }
 
-	public URL getPhotoUrl() {
-		return photoUrl;
-	}
-    
-    
-    
+    public URL getPhotoUrl()
+    {
+        return photoUrl;
+    }
 
- 
 }
-
