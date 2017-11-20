6c8fb24b5c16a318056875558bbabc2ff0a86c84
BhartiL <31671389+BhartiL@users.noreply.github.com>
2017-11-20 14:55:34 -0800

    Add files via upload

---
+++ src/com/neuSep17/service/DealerAPI.java
@@ -0,0 +1,55 @@
+package com.neuSep17.service;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+
+import com.neuSep17.dto.Dealer;
+
+public class DealerAPI {
+	
+	private LinkedHashMap<String, Dealer> DealerMap;
+	private String fileName;
+	
+	public ArrayList<Dealer> getDealers() {
+		return new ArrayList<Dealer>(DealerMap.values());
+	}
+	
+	public DealerAPI(String file) {
+		this.fileName = file;
+		try {
+			DealerMap = getDealersMap(file);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	protected LinkedHashMap<String, Dealer> getDealersMap(String file) throws IOException {
+		File inventoryFile = new File(file);
+		BufferedReader reader = new BufferedReader(new FileReader(inventoryFile));
+		LinkedHashMap<String, Dealer> dealerObjMap = new LinkedHashMap<String, Dealer>();		
+		String line = null;
+		
+		while((line=reader.readLine())!=null) {
+			String[] dealerArray = line.split("\t");
+			Dealer dealer =new Dealer(dealerArray[0],dealerArray[0], dealerArray[2]);
+			DealerMap.put(dealer.getId(), dealer);
+		}
+		reader.close();
+		return dealerObjMap;
+	}
+	
+	public Dealer getDealer(String id) {
+		return DealerMap.get(id);
+		
+	}
+
+	public LinkedHashMap<String, Dealer> getDealerMap() {
+		return DealerMap;
+	}
+	
+
+}
---
+++ src/com/neuSep17/service/InventoryAPI.java
@@ -0,0 +1,64 @@
+package com.neuSep17.dto;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.LinkedHashMap;
+
+import com.neuSep17.service.DealerAPI;
+import com.neuSep17.service.ServiceAPI_Test;
+
+public class InventoryAPI extends DealerAPI{
+	
+
+	private String dataFolderPath;
+	public LinkedHashMap<String, Inventory> dealerInventoryMap;
+	
+	public InventoryAPI(String dataFolderPath) throws IOException {
+		super(dataFolderPath+"/dealers");
+		this.getAllInventory(this.getDealerMap().keySet());
+		
+	}
+	
+	public LinkedHashMap<String, Inventory> getAllInventory(Collection<String> dealerIds) throws IOException  {
+		LinkedHashMap<String, Inventory> dealerInventory = new LinkedHashMap<>();
+		for (String dealerId : dealerIds) {
+			Inventory inventory = new Inventory();
+			LinkedHashMap<String, Vehicle> dealerVehicles = ServiceAPI_Test.getVehiclesMap(dataFolderPath+dealerId);
+			inventory.setDealerId(dealerId);
+			inventory.setVehicles(dealerVehicles.values());
+			dealerInventory.put(dealerId, inventory);
+		}
+		return dealerInventory;
+	}
+	
+	public void addVehicleToDealersInventory(String dealerId, Vehicle vehicle) {
+		Inventory dealerInventory = dealerInventoryMap.get(dealerId);
+		ArrayList<Vehicle> vehicleList = (ArrayList<Vehicle>) dealerInventory.getVehicles();
+		vehicleList.add(vehicle);
+		dealerInventoryMap.put(dealerId, dealerInventory);
+	}
+	
+	public boolean removeVehicleFromDealersInventory(String dealerId, String vehicleId) {
+		Inventory dealerInventory = dealerInventoryMap.get(dealerId);
+		ArrayList<Vehicle> vehicleList = (ArrayList<Vehicle>) dealerInventory.getVehicles();
+		for(int i=0 ; i < vehicleList.size(); i++) {
+			if(vehicleList.get(i).getId().equals(vehicleId)) {
+				vehicleList.remove(i);
+				dealerInventoryMap.put(dealerId, dealerInventory);
+				return true;
+			}
+		}
+		return false;
+	}
+		
+	public boolean updateVehicleInDealersInventory(String dealerId, Vehicle vehicle) {
+		boolean hasVehicle = removeVehicleFromDealersInventory(dealerId, vehicle.getId());
+		if(!hasVehicle)
+			return false;
+		else
+			addVehicleToDealersInventory(dealerId, vehicle);
+		return true;
+	}
+}
