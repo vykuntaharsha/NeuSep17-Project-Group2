2ce7ce576801510c4eaf210047d1b61a0d645562
YujiaNEU2017 <zhang.yujia@husky.neu.edu>
2017-11-24 19:49:01 -0800

    update browse inventory filter

--- src/com/neuSep17/ui/consumer/BrowseInventoryFrame.java
+++ src/com/neuSep17/ui/consumer/BrowseInventoryFrame.java
@@ -7,6 +7,8 @@ import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
 import java.beans.PropertyChangeEvent;
@@ -20,8 +22,11 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import javax.imageio.ImageIO;
 import javax.swing.BorderFactory;
+import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
+import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JList;
@@ -32,39 +37,52 @@ import javax.swing.ListCellRenderer;
 import javax.swing.ListSelectionModel;
 import javax.swing.SwingUtilities;
 import javax.swing.SwingWorker;
+import javax.swing.border.TitledBorder;
 
-import com.neuSep17.dto.Dealer;
 import com.neuSep17.dto.Vehicle;
+
 import java.awt.FlowLayout;
 
-public class BrowseInventoryFrame extends JFrame implements Runnable{
-    
+public class BrowseInventoryFrame extends JFrame implements Runnable
+{
+
     private String dealerID;
     private HashMap<Vehicle, Image> cache;
     private JList<Vehicle> displayList;
     private JScrollPane listScrollPane;
     private JPanel listPanel;
-    
-    public BrowseInventoryFrame() //(Dealer dealer)
+
+    // filter start
+    private JCheckBox[] categories;
+    private JLabel[] filterLabels;
+    private JComboBox[] filters;
+    private String[] data = { "all", "test1", "test2", "test3" };// Simulation
+                                                                 // data
+    // filter end
+
+    public BrowseInventoryFrame() // (Dealer dealer)
     {
-        super(); 
+        super();
         setTitle("Browse Inventory of xx dealer");
-        //dealerID= dealer.getID();
-        
-        this.cache=new HashMap<Vehicle, Image>();
-        try {
+        // dealerID= dealer.getID();
+
+        this.cache = new HashMap<Vehicle, Image>();
+        try
+        {
             updateVehicle();
-        } catch (IOException e) {
+        }
+        catch (IOException e)
+        {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
-        
+
         this.setSize(1200, 700);
-        
+
         createComponents();
         addComponents();
         addListeners();
-        
+
     }
 
     private void createComponents()
@@ -85,27 +103,58 @@ public class BrowseInventoryFrame extends JFrame implements Runnable{
 
     private void createFilterPanelComponents()
     {
-        // TODO Auto-generated method stub
+        categories = new JCheckBox[3];
+        categories[0] = new JCheckBox();
+        categories[0].setText("New");
+        categories[0].setName("new");
+        categories[1] = new JCheckBox();
+        categories[1].setText("Used");
+        categories[1].setName("used");
+        categories[2] = new JCheckBox();
+        categories[2].setText("Certified");
+        categories[2].setName("certified");
 
+        filterLabels = new JLabel[4];
+        filterLabels[0] = new JLabel("Year");
+        filterLabels[1] = new JLabel("Make");
+        filterLabels[2] = new JLabel("Price");
+        filterLabels[3] = new JLabel("Type");
+        filters = new JComboBox[4];
+        filters[0] = new JComboBox<String>();
+        filters[0].setName("year");
+        filters[1] = new JComboBox<String>();
+        filters[1].setName("make");
+        filters[2] = new JComboBox<String>();
+        filters[2].setName("price");
+        filters[3] = new JComboBox<String>();
+        filters[3].setName("type");
+
+        // Simulation data
+        for (int i = 0; i < data.length; i++)
+        {
+            filters[0].addItem("0" + data[i]);
+            filters[1].addItem("1" + data[i]);
+            filters[2].addItem("2" + data[i]);
+            filters[3].addItem("3" + data[i]);
+        }
     }
-   
+
     private void addComponents()
     {
         Container con = getContentPane();
         con.setLayout(new BorderLayout(1, 1));
-        
+
         // search panel
         JPanel searchPanel = new JPanel();
         addSearchPanelComponents(searchPanel);
         con.add(searchPanel, BorderLayout.NORTH);
-        
+
         // filter panel
         JPanel filterPanel = new JPanel();
         addFilterPanelComponents(filterPanel);
         con.add(filterPanel, BorderLayout.WEST);
-        
         // list panel
-        
+
         addListPanelComponents();
         con.add(listPanel, BorderLayout.CENTER);
     }
@@ -117,7 +166,41 @@ public class BrowseInventoryFrame extends JFrame implements Runnable{
 
     private void addFilterPanelComponents(JPanel filterPanel)
     {
-        filterPanel.setBorder(BorderFactory.createLineBorder(Color.black));
+        BoxLayout boxLayout = new BoxLayout(filterPanel, BoxLayout.Y_AXIS);
+        filterPanel.setLayout(boxLayout);
+        filterPanel.setBorder(new TitledBorder("Filter"));
+        JPanel checkBoxPanel = new JPanel();
+        boxLayout = new BoxLayout(checkBoxPanel, BoxLayout.Y_AXIS);
+        checkBoxPanel.setLayout(boxLayout);
+        checkBoxPanel.setBorder(new TitledBorder("Category"));
+        for (int i = 0; i < categories.length; i++)
+        {
+            JPanel linePanel = new JPanel(new FlowLayout(FlowLayout.LEADING));
+            linePanel.add(categories[i]);
+            checkBoxPanel.add(linePanel);
+            checkBoxPanel.add(Box.createVerticalStrut(10));
+        }
+
+        filterPanel.add(Box.createVerticalStrut(10));
+        filterPanel.add(checkBoxPanel);
+        filterPanel.add(Box.createVerticalStrut(30));
+        filterPanel.add(Box.createVerticalGlue());
+
+        JPanel comboBoxPanel = new JPanel();
+        boxLayout = new BoxLayout(comboBoxPanel, BoxLayout.Y_AXIS);
+        comboBoxPanel.setLayout(boxLayout);
+        for (int i = 0; i < filterLabels.length; i++)
+        {
+            JPanel linePanel = new JPanel(new FlowLayout(FlowLayout.LEADING));
+            linePanel.add(filterLabels[i]);
+            linePanel.add(filters[i]);
+            filterLabels[i].setLabelFor(filters[i]);
+            comboBoxPanel.add(linePanel);
+        }
+
+        filterPanel.add(comboBoxPanel);
+        filterPanel.add(Box.createVerticalGlue());
+        filterPanel.add(Box.createVerticalStrut(50));
     }
 
     private void addListeners()
@@ -128,10 +211,10 @@ public class BrowseInventoryFrame extends JFrame implements Runnable{
         addFilterPanelListeners();
         // list panel
         addListPanelListeners();
-        
+
         BrowseWindowListener m = new BrowseWindowListener();
         this.addWindowListener(m);
-        
+
     }
 
     private void addSearchPanelListeners()
@@ -142,213 +225,290 @@ public class BrowseInventoryFrame extends JFrame implements Runnable{
 
     private void addFilterPanelListeners()
     {
-        // TODO Auto-generated method stub
+        FilterListener fl = new FilterListener();
+        for (int i = 0; i < categories.length; i++)
+        {
+            categories[i].addActionListener(fl);
+        }
 
+        for (int i = 0; i < filterLabels.length; i++)
+        {
+            filters[i].addActionListener(fl);
+        }
     }
 
-    
-    
-    private void createListPanelComponents(){
-        listPanel = new JPanel();        
-        listPanel.setSize(new Dimension(this.getWidth()-300, this.getHeight()-200));
-        
+    private void createListPanelComponents()
+    {
+        listPanel = new JPanel();
+        listPanel.setSize(new Dimension(this.getWidth() - 300, this.getHeight() - 200));
+
         displayList = new JList(cache.keySet().toArray());
         displayList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
         displayList.setLayoutOrientation(JList.VERTICAL);
         displayList.setFixedCellHeight(100);
-        
-        int listPanelwidth=listPanel.getWidth();
-        int listPanelheight=listPanel.getHeight();
-        
+
+        int listPanelwidth = listPanel.getWidth();
+        int listPanelheight = listPanel.getHeight();
+
         displayList.setCellRenderer(new vehicleCellRenderer());
-        listScrollPane=new JScrollPane();
+        listScrollPane = new JScrollPane();
         listScrollPane.setViewportView(displayList);
-        listScrollPane.setPreferredSize(new Dimension(listPanelwidth,listPanelheight));
+        listScrollPane.setPreferredSize(new Dimension(listPanelwidth, listPanelheight));
 
     }
-    
+
     private void addListPanelComponents()
     {
         listPanel.setBorder(BorderFactory.createLineBorder(Color.black));
         listPanel.add(listScrollPane);
     }
-    
-    private void addListPanelListeners(){
-        
-        //updateFinishedListener updateFinished=new updateFinishedListener();       
+
+    private void addListPanelListeners()
+    {
+
+        // updateFinishedListener updateFinished=new updateFinishedListener();
 
     }
-    
-    class vehicleCellRenderer implements ListCellRenderer<Vehicle>{
-        
+
+    class vehicleCellRenderer implements ListCellRenderer<Vehicle>
+    {
 
         @Override
-        public Component getListCellRendererComponent(JList<? extends Vehicle> list, Vehicle v, int index, 
-                boolean isSelected, boolean cellHasFocus) {
-            
-            JPanel specPane=new JPanel();
+        public Component getListCellRendererComponent(JList<? extends Vehicle> list, Vehicle v, int index,
+                boolean isSelected, boolean cellHasFocus)
+        {
+
+            JPanel specPane = new JPanel();
             specPane.setLayout(new BoxLayout(specPane, BoxLayout.Y_AXIS));
-            
-            JLabel nameLabel=new JLabel(v.getYear()+" "+v.getMake()+" "+v.getModel());
+
+            JLabel nameLabel = new JLabel(v.getYear() + " " + v.getMake() + " " + v.getModel());
             nameLabel.setFont(new Font(nameLabel.getFont().getFontName(), Font.BOLD, 24));
-            
-            JLabel priceLabel=new JLabel("price: "+v.getPrice().toString());
-            JLabel categoryLabel=new JLabel("category: "+v.getCategory().toString());
-            JLabel trimLabel=new JLabel("trim: "+v.getTrim());
-            JLabel typeLabel=new JLabel("type: "+v.getBodyType());            
-            
-            ImageIcon icon = createTestImageIcon(cache.get(v),"test");            
-            JLabel image=new JLabel(icon);
-            
+
+            JLabel priceLabel = new JLabel("price: " + v.getPrice().toString());
+            JLabel categoryLabel = new JLabel("category: " + v.getCategory().toString());
+            JLabel trimLabel = new JLabel("trim: " + v.getTrim());
+            JLabel typeLabel = new JLabel("type: " + v.getBodyType());
+
+            ImageIcon icon = createTestImageIcon(cache.get(v), "test");
+            JLabel image = new JLabel(icon);
+
             specPane.add(nameLabel);
             specPane.add(priceLabel);
             specPane.add(categoryLabel);
             specPane.add(trimLabel);
             specPane.add(typeLabel);
-            
+
             JSplitPane cell = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, image, specPane);
             cell.setDividerLocation(150);
-            //cell.setSize(list.getWidth(), 100);
+            // cell.setSize(list.getWidth(), 100);
             return cell;
         }
-        
+
     }
-    
-    class updateFinishedListener implements PropertyChangeListener{
+
+    class updateFinishedListener implements PropertyChangeListener
+    {
         @Override
-        public void propertyChange(PropertyChangeEvent evt) {
-            
-        }        
+        public void propertyChange(PropertyChangeEvent evt)
+        {
+
+        }
     }
-    
-    private void updateVehicle() throws IOException {
-        
-        File file = new File("D:/PROF/GIT/INFO5100/INFO5100/assignment8/q3.txt");
-        ArrayList<Vehicle> cars=new ArrayList<>();
-        FileReader fr=new FileReader(file);
-        BufferedReader br=new BufferedReader(fr);
-        String line=br.readLine();
-        while(true){
-            line=br.readLine();
-            if(line==null) break;            
-            Vehicle cur=new Vehicle(line.split("~"));
+
+    private void updateVehicle() throws IOException
+    {
+
+        File file = new File("/Users/zhangyujia/Downloads/q3.txt");
+        ArrayList<Vehicle> cars = new ArrayList<>();
+        FileReader fr = new FileReader(file);
+        BufferedReader br = new BufferedReader(fr);
+        String line = br.readLine();
+        while (true)
+        {
+            line = br.readLine();
+            if (line == null)
+                break;
+            Vehicle cur = new Vehicle(line.split("~"));
             cars.add(cur);
         }
         br.close();
-        fr.close(); 
-        URL imgURL=new URL("http://inventory-dmg.assets-cdk.com/chrome_jpgs/2016/24174x90.jpg");
-        Image temp=ImageIO.read(imgURL);
-        for(Vehicle v: cars) {
-            //cache.put(v, ImageIO.read(v.getPhotoUrl()) );  
+        fr.close();
+        URL imgURL = new URL("http://inventory-dmg.assets-cdk.com/chrome_jpgs/2016/24174x90.jpg");
+        Image temp = ImageIO.read(imgURL);
+        for (Vehicle v : cars)
+        {
+            // cache.put(v, ImageIO.read(v.getPhotoUrl()) );
             cache.put(v, temp);
         }
         return;
-        
+
         /*
-        updateThread t=new updateThread();
-        t.execute();
-        while(t.isDone()) {}            // this waits until update finish
-        */
+         * updateThread t=new updateThread(); t.execute(); while(t.isDone()) {}
+         * // this waits until update finish
+         */
     }
-    
-    protected ImageIcon createTestImageIcon(Image img, String description) {
+
+    protected ImageIcon createTestImageIcon(Image img, String description)
+    {
         return new ImageIcon(img, description);
     }
-    
-    
-    protected ImageIcon createImageIcon(URL imgURL, String description) throws IOException {        
-        if (imgURL != null) {
-            Image img=ImageIO.read(imgURL);
+
+    protected ImageIcon createImageIcon(URL imgURL, String description) throws IOException
+    {
+        if (imgURL != null)
+        {
+            Image img = ImageIO.read(imgURL);
             img.getScaledInstance(90, 90, Image.SCALE_SMOOTH);
             return new ImageIcon(imgURL, description);
-        } else {
+        }
+        else
+        {
             System.err.println("Couldn't find file: " + imgURL);
             return null;
         }
     }
-    
-    class updateThread extends SwingWorker<Void,Void> {
+
+    class updateThread extends SwingWorker<Void, Void>
+    {
 
         @Override
-        protected Void doInBackground() throws Exception {
-            for(Vehicle v: getInventoryofDealer()) {
-                cache.put(v, ImageIO.read(v.getPhotoUrl()) );   //null url situation?
+        protected Void doInBackground() throws Exception
+        {
+            for (Vehicle v : getInventoryofDealer())
+            {
+                cache.put(v, ImageIO.read(v.getPhotoUrl())); // null url
+                                                             // situation?
             }
             return null;
         }
 
-        private ArrayList<Vehicle> getInventoryofDealer() throws IOException {
+        private ArrayList<Vehicle> getInventoryofDealer() throws IOException
+        {
             File file = new File("assignment8/q3.txt");
-            ArrayList<Vehicle> cars=new ArrayList<>();
-            FileReader fr=new FileReader(file);
-            BufferedReader br=new BufferedReader(fr);
-            String line=br.readLine();
-            while(true){
-                line=br.readLine();
-                if(line==null) break;            
-                Vehicle cur=new Vehicle(line.split("~"));
+            ArrayList<Vehicle> cars = new ArrayList<>();
+            FileReader fr = new FileReader(file);
+            BufferedReader br = new BufferedReader(fr);
+            String line = br.readLine();
+            while (true)
+            {
+                line = br.readLine();
+                if (line == null)
+                    break;
+                Vehicle cur = new Vehicle(line.split("~"));
                 cars.add(cur);
             }
             br.close();
             fr.close();
             return cars;
-        }        
+        }
     }
-    
-    
 
     private void makeThisVisible()
     {
-        
         this.setVisible(true);
     }
-    
-    class BrowseWindowListener implements WindowListener {
+
+    class FilterListener implements ActionListener
+    {
+        @Override
+        public void actionPerformed(ActionEvent e)
+        {
+            updateFilter();
+        }
+    }
+
+    private void updateFilter()
+    {
+        StringBuilder sb = new StringBuilder();
+        int count = 0;
+        sb.append("categories:");
+        for (int i = 0; i < categories.length; i++)
+        {
+            if (categories[i].isSelected())
+            {
+                sb.append(categories[i].getName()).append(",");
+                count++;
+            }
+        }
+        if (count == 0 || count == categories.length)
+        {
+            sb.setLength(0);
+        }
+        else
+        {
+            sb.deleteCharAt(sb.length() - 1);
+            sb.append(";");
+        }
+
+        for (int i = 0; i < filters.length; i++)
+        {
+            if (filters[i].getSelectedIndex() == 0)
+            {
+                continue;
+            }
+            else
+            {
+                sb.append(filters[i].getName()).append(":").append(filters[i].getSelectedItem()).append(";");
+            }
+        }
+        System.out.println(sb.toString());
+    }
+
+    class BrowseWindowListener implements WindowListener
+    {
 
         @Override
-        public void windowActivated(WindowEvent arg0) {
-            
+        public void windowActivated(WindowEvent arg0)
+        {
+
         }
 
         @Override
-        public void windowClosed(WindowEvent e) {
-            
+        public void windowClosed(WindowEvent e)
+        {
+
         }
 
         @Override
-        public void windowClosing(WindowEvent e) {           
+        public void windowClosing(WindowEvent e)
+        {
             System.out.println("BrowseInventory closing, back to consumer window.");
         }
 
         @Override
-        public void windowDeactivated(WindowEvent e) {
-            
+        public void windowDeactivated(WindowEvent e)
+        {
+
         }
 
         @Override
-        public void windowDeiconified(WindowEvent e) {
+        public void windowDeiconified(WindowEvent e)
+        {
         }
 
         @Override
-        public void windowIconified(WindowEvent e) {
-            
+        public void windowIconified(WindowEvent e)
+        {
+
         }
 
         @Override
-        public void windowOpened(WindowEvent e) {
+        public void windowOpened(WindowEvent e)
+        {
         }
-        
+
     }
 
     public static void main(String[] args)
-    {        
+    {
         BrowseInventoryFrame bif = new BrowseInventoryFrame();
-        Thread BrowseInventoryThread = new Thread(() -> bif.run() );
+        Thread BrowseInventoryThread = new Thread(() -> bif.run());
         SwingUtilities.invokeLater(BrowseInventoryThread);
     }
 
     @Override
-    public void run() {        
-        makeThisVisible();        
+    public void run()
+    {
+        makeThisVisible();
     }
 }
