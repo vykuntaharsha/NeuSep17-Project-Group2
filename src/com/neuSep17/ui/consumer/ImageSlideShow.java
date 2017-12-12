72abd504e5665fa4dafdf2d38d79f8ca15fef804
YujiaNEU2017 <zhang.yujia@husky.neu.edu>
2017-12-12 14:30:43 -0800

    update -> vehicleDetails get images

--- src/com/neuSep17/ui/consumer/ImageSlideShow.java
+++ src/com/neuSep17/ui/consumer/ImageSlideShow.java
@@ -8,191 +8,178 @@ import java.awt.Image;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.image.BufferedImage;
-import java.io.File;
 import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
-import java.util.List;
 import java.util.Map;
 
-import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 import com.neuSep17.dto.Vehicle;
+import com.neuSep17.service.InventoryServiceAPI_Test;
 
-public class ImageSlideShow extends JPanel implements ActionListener {
-	private List<URL> urls = new ArrayList<>();
-	private String[] buttonNames = { "first", "pre", "next", "last" };
-	private int currentImage = 0;
-	private int width = 600, height = 550;
-	private JPanel buttonPanel = new JPanel();
-	
+public class ImageSlideShow extends JPanel implements ActionListener
+{
+    private ArrayList<Image> myimages = new ArrayList<>();
+    Vehicle vehicle;
+    private String[] buttonNames = { "first", "pre", "next", "last" };
+    private int currIndex = 0;
+    private int width = 600, height = 550;
+    private JPanel buttonPanel = new JPanel();
 
-	private GridBagLayout gbl = new GridBagLayout();
-	private GridBagConstraints gbs = new GridBagConstraints();
+    private GridBagLayout gbl = new GridBagLayout();
+    private GridBagConstraints gbs = new GridBagConstraints();
 
-	Map<JButton, String> buttons = new LinkedHashMap<>();
-	JLabel imageLabel;
-	JLabel title;
-	Map<Integer, ImageIcon> images = new HashMap<>();
+    Map<JButton, String> buttons = new LinkedHashMap<>();
+    JLabel imageLabel;
+    JLabel title;
+    Map<Integer, ImageIcon> images = new HashMap<>();
 
-	public ImageSlideShow(Vehicle vehicle, String[] imageUrls) {
+    public ImageSlideShow(Vehicle vehicle, ArrayList<Image> myimages)
+    {
+        this.vehicle = vehicle;
+        createComponents(vehicle, myimages);
+        addComponents();
+        this.setVisible(true);
+    }
 
-		createComponents(vehicle, imageUrls);
-		addComponents();
-		this.setVisible(true);
-	}
+    private void addComponents()
+    {
+        this.setLayout(gbl);
+        title.setHorizontalAlignment(JLabel.LEFT);
+        this.add(title);
+        this.add(imageLabel);
+        for (JButton jButton : buttons.keySet())
+        {
+            buttonPanel.add(jButton);
+        }
+        this.add(buttonPanel);
 
-	private void addComponents() {
-		this.setLayout(gbl);
-		title.setHorizontalAlignment(JLabel.LEFT);
-		this.add(title);
-		this.add(imageLabel);
-		for (JButton jButton : buttons.keySet()) {
-			buttonPanel.add(jButton);
-		}
-		this.add(buttonPanel);
+        gbs.fill = GridBagConstraints.BOTH;
+        gbs.gridwidth = 1;
+        gbs.gridheight = 1;
+        gbs.insets = new Insets(0, 30, 0, 0);
+        gbs.weightx = 1;
+        gbs.weighty = 1;
+        gbs.gridx = 0;
+        gbs.gridy = 0;
+        gbl.setConstraints(title, gbs);
 
-		gbs.fill = GridBagConstraints.BOTH;
-		gbs.gridwidth = 1;
-		gbs.gridheight = 1;
-		gbs.insets = new Insets(0, 30, 0, 0);
-		gbs.weightx = 1;
-		gbs.weighty = 1;
-		gbs.gridx = 0;
-		gbs.gridy = 0;
-		gbl.setConstraints(title, gbs);
+        gbs.fill = GridBagConstraints.BOTH;
+        gbs.gridwidth = 1;
+        gbs.gridheight = 1;
+        gbs.insets = new Insets(0, 30, 0, 0);
+        gbs.weightx = 1;
+        gbs.weighty = 1;
+        gbs.gridx = 0;
+        gbs.gridy = 1;
+        gbl.setConstraints(imageLabel, gbs);
 
-		gbs.fill = GridBagConstraints.BOTH;
-		gbs.gridwidth = 1;
-		gbs.gridheight = 1;
-		gbs.insets = new Insets(0, 30, 0, 0);
-		gbs.weightx = 1;
-		gbs.weighty = 1;
-		gbs.gridx = 0;
-		gbs.gridy = 1;
-		gbl.setConstraints(imageLabel, gbs);
+        gbs.fill = GridBagConstraints.BOTH;
+        gbs.gridwidth = 1;
+        gbs.gridheight = 1;
+        gbs.insets = new Insets(0, 0, 0, 0);
+        gbs.weightx = 1;
+        gbs.weighty = 1;
+        gbs.gridx = 0;
+        gbs.gridy = 2;
+        gbl.setConstraints(buttonPanel, gbs);
+    }
 
-		gbs.fill = GridBagConstraints.BOTH;
-		gbs.gridwidth = 1;
-		gbs.gridheight = 1;
-		gbs.insets = new Insets(0, 0, 0, 0);
-		gbs.weightx = 1;
-		gbs.weighty = 1;
-		gbs.gridx = 0;
-		gbs.gridy = 2;
-		gbl.setConstraints(buttonPanel, gbs);
-	}
+    private void createComponents(Vehicle vehicle, ArrayList<Image> myimages)
+    {
 
-	private void createComponents(Vehicle vehicle, String[] imageUrls) {
-		title = new JLabel();
-		imageLabel = new JLabel();
+        title = new JLabel();
+        imageLabel = new JLabel();
 
-		for (String buttonName : buttonNames) {
-			JButton button = new JButton(buttonName);
-			button.addActionListener(this);
-			buttons.put(button, buttonName);
-		}
+        for (String buttonName : buttonNames)
+        {
+            JButton button = new JButton(buttonName);
+            button.addActionListener(this);
+            buttons.put(button, buttonName);
+        }
 
-		for (String spec : imageUrls) {
-//			if (isImage(spec)) {
-//
-//			} else {
-//				spec = null;
-//			}
-			if(!isImage(spec)){
-				spec = null;
-			}
-			try {
-				urls.add(new URL(spec));
-			} catch (MalformedURLException e) {
-				urls.add(null);
-			}
-		}
-
-		loadImage();
+        loadImage(myimages);
         title.setText(vehicle.getCategory().toString());
         title.setForeground(Color.RED);
-//		title.setText(vehicle.getCategory() + " " + vehicle.getYear() + " " + vehicle.getMake() + " "
-//				+ vehicle.getModel() + " " + vehicle.getTrim());
-		String s=" " + vehicle.getYear() + " " + vehicle.getMake() + " "
-				+ vehicle.getModel() + " " + vehicle.getTrim();
-		title.setText(String.format("<html>%s<font color='black'>%s</font></html>", 
-		        title.getText(),s));
-		title.setFont(new Font("Menu.font", Font.PLAIN, 20));
-		
-		
-	}
+        String s = " " + vehicle.getYear() + " " + vehicle.getMake() + " " + vehicle.getModel() + " "
+                + vehicle.getTrim();
+        title.setText(String.format("<html>%s<font color='black'>%s</font></html>", title.getText(), s));
+        title.setFont(new Font("Menu.font", Font.PLAIN, 20));
 
-	private void loadImage() {
-		URL url = urls.get(currentImage);
-		ImageIcon icon;
-		BufferedImage img = null;
-		if (url != null) {
-//			icon = new ImageIcon(urls.get(currentImage));
-			icon = new ImageIcon(url);
-		} else {
+    }
 
-			try {
-				img = ImageIO.read(new File("src/com/neuSep17/ui/consumer/imagenotfound.jpg"));
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			icon = new ImageIcon(img);
+    private void loadImage(ArrayList<Image> myimages)
+    {
+        ImageIcon icon = new ImageIcon(myimages.get(currIndex));
+        icon.setImage(icon.getImage().getScaledInstance(width, height, Image.SCALE_DEFAULT));
 
-		}
-		// ImageIcon icon=new ImageIcon(urls.get(currentImage));
+        if (!images.containsKey(currIndex))
+        {
+            images.put(currIndex, icon);
+        }
+        imageLabel.setIcon(images.get(currIndex));
+    }
 
-		icon.setImage(icon.getImage().getScaledInstance(width, height, Image.SCALE_DEFAULT));
+    @Override
+    public void actionPerformed(ActionEvent e)
+    {
+        String name = buttons.get(e.getSource());
+        if (name.equals("next"))
+        {
+            this.currIndex = (this.currIndex + 1) % 3;
+            try
+            {
+                myimages = InventoryServiceAPI_Test.getVehicleImage(vehicle.getBodyType());
+            }
+            catch (IOException e1)
+            {
+                e1.printStackTrace();
+            }
 
-		if (!images.containsKey(currentImage)) {
-			images.put(currentImage, icon);
-		}
-		imageLabel.setIcon(images.get(currentImage));
-	}
+        }
+        else if (name.equals("pre"))
+        {
+            this.currIndex = (this.currIndex + 2) % 3;
+            try
+            {
+                myimages = InventoryServiceAPI_Test.getVehicleImage(vehicle.getBodyType());
+            }
+            catch (IOException e1)
+            {
+                e1.printStackTrace();
+            }
 
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		String name = buttons.get(e.getSource());
-		if (name.equals("next")) {
-			this.currentImage = (this.currentImage + 1) % this.urls.size();
+        }
+        else if (name.equals("first"))
+        {
+            this.currIndex = 0;
+            try
+            {
+                myimages = InventoryServiceAPI_Test.getVehicleImage(vehicle.getBodyType());
+            }
+            catch (IOException e1)
+            {
+                e1.printStackTrace();
+            }
 
-		} else if (name.equals("pre")) {
-			this.currentImage = (this.currentImage - 1 + this.urls.size()) % this.urls.size();
-
-		} else if (name.equals("first")) {
-			this.currentImage = 0;
-
-		} else if (name.equals("last")) {
-			this.currentImage = this.urls.size() - 1;
-
-		}
-		loadImage();
-	}
-
-	private boolean isImage(String image_path) {
-		Image image=null;
-		try {
-			image = ImageIO.read(new URL(image_path));
-		} catch (MalformedURLException e) {
-			// TODO Auto-generated catch block
-//			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-//			e.printStackTrace();
-		}
-		if(image != null){
-		    return true;
-		}else{
-		    return false;
-		}
-	}
+        }
+        else if (name.equals("last"))
+        {
+            this.currIndex = 2;
+            try
+            {
+                myimages = InventoryServiceAPI_Test.getVehicleImage(vehicle.getBodyType());
+            }
+            catch (IOException e1)
+            {
+                e1.printStackTrace();
+            }
+        }
+        loadImage(myimages);
+    }
 }
--- src/com/neuSep17/ui/consumer/VehicleDetailDialog.java
+++ src/com/neuSep17/ui/consumer/VehicleDetailDialog.java
@@ -2,7 +2,10 @@ package com.neuSep17.ui.consumer;
 
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.Image;
 import java.awt.Insets;
+import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.swing.JDialog;
 import javax.swing.JFrame;
@@ -11,54 +14,63 @@ import com.neuSep17.dto.Vehicle;
 import com.neuSep17.service.IncentiveServiceAPI_Test;
 import com.neuSep17.service.InventoryServiceAPI_Test;
 
-public class VehicleDetailDialog extends JDialog {
-	private GridBagLayout gbl = new GridBagLayout();
-	private GridBagConstraints gbs = new GridBagConstraints();
+public class VehicleDetailDialog extends JDialog
+{
+    private GridBagLayout gbl = new GridBagLayout();
+    private GridBagConstraints gbs = new GridBagConstraints();
 
-	public VehicleDetailDialog(JFrame owner, String id, IncentiveServiceAPI_Test incentiveServiceAPI_Test,
-			InventoryServiceAPI_Test inventoryServiceAPI_Test) {
-		super(owner, "VEHICLE DETAILS", false);
-		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-		addComponents(id, incentiveServiceAPI_Test, inventoryServiceAPI_Test);
-		this.setSize(1000, 1000);
-		this.setVisible(true);
-		
-	}
+    public VehicleDetailDialog(JFrame owner, String id, IncentiveServiceAPI_Test incentiveServiceAPI_Test,
+            InventoryServiceAPI_Test inventoryServiceAPI_Test)
+    {
+        super(owner, "VEHICLE DETAILS", false);
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        addComponents(id, incentiveServiceAPI_Test, inventoryServiceAPI_Test);
+        this.setSize(1000, 1000);
+        this.setVisible(true);
 
-	private void addComponents(String id, IncentiveServiceAPI_Test incentiveServiceAPI_Test,
-			InventoryServiceAPI_Test inventoryServiceAPI_Test) {
-		Vehicle vehicle = inventoryServiceAPI_Test.getVehicleDetails(id);
+    }
 
-//		String[] urls = { vehicle.getPhotoUrl().toString() };  //TODO  NullPointer may happen when read from another dealer, don't know why
-    	String[] urls={"http://inventory-cf.assets-cdk.com/0/6/0/15889666060x640.jpg","http://inventory-cf.assets-cdk.com/1/6/1/15889666161x640.jpg","http://inventory-cf.assets-cdk.com/5/8/2/15889666285x640.jpg"};
-		ImageSlideShow imageSlideShow = new ImageSlideShow(vehicle, urls);
-		DetailShow detailShow = new DetailShow(vehicle, incentiveServiceAPI_Test);
-		this.setLayout(gbl);
+    private void addComponents(String id, IncentiveServiceAPI_Test incentiveServiceAPI_Test,
+            InventoryServiceAPI_Test inventoryServiceAPI_Test)
+    {
+        Vehicle vehicle = inventoryServiceAPI_Test.getVehicleDetails(id);
+        ArrayList<Image> myimages = new ArrayList<Image>();
+        try
+        {
+            myimages = InventoryServiceAPI_Test.getVehicleImage(vehicle.getBodyType());
+        }
+        catch (IOException e)
+        {
+            System.out.println("Cannot get images.");
+            e.printStackTrace();
+        }
+        ImageSlideShow imageSlideShow = new ImageSlideShow(vehicle, myimages);
+        DetailShow detailShow = new DetailShow(vehicle, incentiveServiceAPI_Test);
+        this.setLayout(gbl);
 
-		this.getContentPane().add(imageSlideShow);
-		this.getContentPane().add(detailShow);
+        this.getContentPane().add(imageSlideShow);
+        this.getContentPane().add(detailShow);
 
-		gbs.fill = GridBagConstraints.BOTH;
-		gbs.gridwidth = 1;
-		gbs.gridheight = 1;
-		gbs.insets = new Insets(30, 20, 0, 0);
-		gbs.weightx = 1;
-		gbs.weighty = 1;
-		gbs.gridx = 0;
-		gbs.gridy = 0;
-		gbl.setConstraints(imageSlideShow, gbs);
+        gbs.fill = GridBagConstraints.BOTH;
+        gbs.gridwidth = 1;
+        gbs.gridheight = 1;
+        gbs.insets = new Insets(30, 20, 0, 0);
+        gbs.weightx = 1;
+        gbs.weighty = 1;
+        gbs.gridx = 0;
+        gbs.gridy = 0;
+        gbl.setConstraints(imageSlideShow, gbs);
 
-		gbs.fill = GridBagConstraints.BOTH;
-		gbs.gridwidth = 3;
-		gbs.gridheight = 1;
-		gbs.insets = new Insets(50, 10, 105, 50);
-		gbs.weightx = 2;
-		gbs.weighty = 1;
-		gbs.gridx = 1;
-		gbs.gridy = 0;
-		gbl.setConstraints(detailShow, gbs);
-		setVisible(true);
-	}	
-	
+        gbs.fill = GridBagConstraints.BOTH;
+        gbs.gridwidth = 3;
+        gbs.gridheight = 1;
+        gbs.insets = new Insets(50, 10, 105, 50);
+        gbs.weightx = 2;
+        gbs.weighty = 1;
+        gbs.gridx = 1;
+        gbs.gridy = 0;
+        gbl.setConstraints(detailShow, gbs);
+        setVisible(true);
+    }
 
 }
