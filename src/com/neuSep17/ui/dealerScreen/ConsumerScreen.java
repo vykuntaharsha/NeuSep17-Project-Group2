306f8bc163d03b82b5266ac2bea96a3ec9c2dd7e
kevinshi721 <kevinshi721@gmail.com>
2017-12-11 20:02:32 -0800

    two option when connect to InventoryBrowse

--- src/com/neuSep17/ui/dealerScreen/ConsumerScreen.java
+++ src/com/neuSep17/ui/dealerScreen/ConsumerScreen.java
@@ -12,6 +12,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.function.Consumer;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -20,6 +21,7 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.SwingUtilities;
+import javax.swing.text.html.Option;
 
 import com.neuSep17.dto.Dealer;
 import com.neuSep17.service.DealerAPI;
@@ -36,7 +38,7 @@ public class ConsumerScreen  extends JFrame{
     private ArrayList<String> dealerNameList = new ArrayList<>();
     
     public ConsumerScreen() throws HeadlessException {
-    		super();
+        super();
         setTitle("Consumer Screen");
         createComponents();
         addComponentsUsingGridBagLayout();
@@ -46,125 +48,125 @@ public class ConsumerScreen  extends JFrame{
     
     private void createComponents() {
 
-      browser = new JButton("Browse Inventory");
-      browser.setFont(new Font("Default", Font.PLAIN, 20));
-      browser.setForeground(Color.DARK_GRAY);
-      browser.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+        browser = new JButton("Browse Inventory");
+        browser.setFont(new Font("Default", Font.PLAIN, 20));
+        browser.setForeground(Color.DARK_GRAY);
+        browser.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
 
-      browserAll = new JButton("Browse All Dealers' Inventory");
-      browserAll.setFont(new Font("Default", Font.PLAIN, 20));
-      browserAll.setForeground(Color.DARK_GRAY);
-      browserAll.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+        browserAll = new JButton("Browse All Dealers' Inventory");
+        browserAll.setFont(new Font("Default", Font.PLAIN, 20));
+        browserAll.setForeground(Color.DARK_GRAY);
+        browserAll.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
 
-      first = new JLabel ("Please Select a Dealer ", JLabel.CENTER);
-      first.setFont(new Font("Default",Font.BOLD, 55));
-      first.setForeground(Color.DARK_GRAY);
-      
-      second = new JLabel("Dealer Name", JLabel.RIGHT);
-      second.setFont(new Font("Default",Font.PLAIN, 20));
-      second.setForeground(Color.DARK_GRAY);
+        first = new JLabel ("Please Select a Dealer ", JLabel.CENTER);
+        first.setFont(new Font("Default",Font.BOLD, 55));
+        first.setForeground(Color.DARK_GRAY);
 
-      third = new JLabel("***** Have No Idea About Dealers? *****", JLabel.CENTER);
-      third.setFont(new Font("Default",Font.ITALIC, 20));
-      third.setForeground(Color.DARK_GRAY);
-      
-      exitButton = new JButton("Exit");
-      exitButton.setFont(new Font("Default", Font.PLAIN, 20));
-      exitButton.setForeground(Color.DARK_GRAY);
+        second = new JLabel("Dealer Name", JLabel.RIGHT);
+        second.setFont(new Font("Default",Font.PLAIN, 20));
+        second.setForeground(Color.DARK_GRAY);
 
-      for (Dealer d: dealerList){
+        third = new JLabel("***** Have No Idea About Dealers? *****", JLabel.CENTER);
+        third.setFont(new Font("Default",Font.ITALIC, 20));
+        third.setForeground(Color.DARK_GRAY);
+
+        exitButton = new JButton("Exit");
+        exitButton.setFont(new Font("Default", Font.PLAIN, 20));
+        exitButton.setForeground(Color.DARK_GRAY);
+
+        for (Dealer d: dealerList){
           dealerNameList.add(d.getId());
-      }
+        }
 
-      combobox=new JComboBox(dealerNameList.toArray());
-      combobox.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
-      combobox.setSelectedItem(null);
+        combobox=new JComboBox(dealerNameList.toArray());
+        combobox.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+        combobox.setSelectedItem(null);
 
   }
 
   public void addComponentsUsingGridBagLayout(){
 	  
-	  ImageIcon backgroundImage = new ImageIcon("data/images/DealerBackGround.jpg");
-	  JLabel backgroundLabel = new JLabel(backgroundImage);
-	  this.add(backgroundLabel);
-	  
-	  backgroundLabel.setSize(backgroundImage.getIconWidth(), backgroundImage.getIconHeight());
-      backgroundLabel.setLocation(0, 0);
-      backgroundLabel.setLayout(new GridBagLayout());
-      
-      GridBagConstraints gbc = new GridBagConstraints();
+        ImageIcon backgroundImage = new ImageIcon("data/images/DealerBackGround.jpg");
+        JLabel backgroundLabel = new JLabel(backgroundImage);
+        this.add(backgroundLabel);
 
-      gbc.gridwidth = 3;
-      gbc.gridheight = 1;
-      gbc.gridx = 0;
-      gbc.gridy = 0;
-      gbc.fill = GridBagConstraints.HORIZONTAL;
-      gbc.insets = new Insets(50, 0, 140, 0);
-      backgroundLabel.add(first, gbc);
+        backgroundLabel.setSize(backgroundImage.getIconWidth(), backgroundImage.getIconHeight());
+        backgroundLabel.setLocation(0, 0);
+        backgroundLabel.setLayout(new GridBagLayout());
 
-      gbc.gridwidth = 1;
-      gbc.gridheight = 1;
-      gbc.gridx = 0;
-      gbc.gridy = 1;
-      gbc.anchor = GridBagConstraints.LINE_END;
-      gbc.fill = GridBagConstraints.VERTICAL;
-      gbc.insets = new Insets(0, 0, 0, 0);
-      backgroundLabel.add(second, gbc);
+        GridBagConstraints gbc = new GridBagConstraints();
 
-      gbc.gridwidth = 1;
-      gbc.gridheight = 1;
-      gbc.gridx = 1;
-      gbc.gridy = 1;
-      gbc.ipadx = 200;
-      gbc.anchor = GridBagConstraints.CENTER;
-      backgroundLabel.add(combobox, gbc);
-      
-      gbc.gridwidth = 1;
-      gbc.gridheight = 1;
-      gbc.gridx = 2;
-      gbc.gridy = 1;
-      gbc.ipadx = 0;
-      gbc.anchor = GridBagConstraints.LINE_START;
-      backgroundLabel.add(browser, gbc);
-      
-      gbc.gridwidth = 1;
-      gbc.gridheight = 1;
-      gbc.gridx = 1;
-      gbc.gridy = 2;
-      gbc.anchor = GridBagConstraints.CENTER;
-      gbc.insets = new Insets(100, 0, 10, 0);
-      backgroundLabel.add(third, gbc);
-      
-      gbc.gridwidth = 1;
-      gbc.gridheight = 1;
-      gbc.gridx = 1;
-      gbc.gridy = 3;
-      gbc.fill = GridBagConstraints.HORIZONTAL;
-      gbc.insets = new Insets(0, 0, 0, 0);
-      backgroundLabel.add(browserAll, gbc);
-      
-      gbc.gridwidth = 1;
-      gbc.gridheight = 1;
-      gbc.gridx = 1;
-      gbc.gridy = 4;
-      gbc.anchor = GridBagConstraints.CENTER;
-      gbc.insets = new Insets(110, 0, 50, 0);
-      backgroundLabel.add(exitButton, gbc);
+        gbc.gridwidth = 3;
+        gbc.gridheight = 1;
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.insets = new Insets(50, 0, 140, 0);
+        backgroundLabel.add(first, gbc);
+
+        gbc.gridwidth = 1;
+        gbc.gridheight = 1;
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        gbc.anchor = GridBagConstraints.LINE_END;
+        gbc.fill = GridBagConstraints.VERTICAL;
+        gbc.insets = new Insets(0, 0, 0, 0);
+        backgroundLabel.add(second, gbc);
+
+        gbc.gridwidth = 1;
+        gbc.gridheight = 1;
+        gbc.gridx = 1;
+        gbc.gridy = 1;
+        gbc.ipadx = 200;
+        gbc.anchor = GridBagConstraints.CENTER;
+        backgroundLabel.add(combobox, gbc);
+
+        gbc.gridwidth = 1;
+        gbc.gridheight = 1;
+        gbc.gridx = 2;
+        gbc.gridy = 1;
+        gbc.ipadx = 0;
+        gbc.anchor = GridBagConstraints.LINE_START;
+        backgroundLabel.add(browser, gbc);
+
+        gbc.gridwidth = 1;
+        gbc.gridheight = 1;
+        gbc.gridx = 1;
+        gbc.gridy = 2;
+        gbc.anchor = GridBagConstraints.CENTER;
+        gbc.insets = new Insets(100, 0, 10, 0);
+        backgroundLabel.add(third, gbc);
+
+        gbc.gridwidth = 1;
+        gbc.gridheight = 1;
+        gbc.gridx = 1;
+        gbc.gridy = 3;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.insets = new Insets(0, 0, 0, 0);
+        backgroundLabel.add(browserAll, gbc);
+
+        gbc.gridwidth = 1;
+        gbc.gridheight = 1;
+        gbc.gridx = 1;
+        gbc.gridy = 4;
+        gbc.anchor = GridBagConstraints.CENTER;
+        gbc.insets = new Insets(110, 0, 50, 0);
+        backgroundLabel.add(exitButton, gbc);
 
   }
 
   private void addListener() {
-      Browser browserCar = new Browser();
-      browser.addActionListener(browserCar);
-      BrowserAll browser = new BrowserAll();
-      browserAll.addActionListener(browser);
-      ExitAL exitAL = new ExitAL();
-      exitButton.addActionListener(exitAL);
+        Browser browserCar = new Browser();
+        browser.addActionListener(browserCar);
+        BrowserAll browser = new BrowserAll();
+        browserAll.addActionListener(browser);
+        ExitAL exitAL = new ExitAL();
+        exitButton.addActionListener(exitAL);
   }
 
   public Dealer getSelectedDealer(){
-      String dealerID = combobox.getSelectedItem().toString();
-      return dealerAPI.getDealer(dealerID);
+        String dealerID = combobox.getSelectedItem().toString();
+        return dealerAPI.getDealer(dealerID);
   }
 
   class Browser implements ActionListener {
@@ -178,7 +180,12 @@ public class ConsumerScreen  extends JFrame{
               Thread BrowseInventoryThread = new Thread(() -> bif.run());
               SwingUtilities.invokeLater(BrowseInventoryThread);
 
+//              Option 1:
+//              new ConsumerScreen().setVisible(false);
+
+//              Option 2:
               dispose();
+
           } else {
               JOptionPane.showMessageDialog(null, "Please Select a Dealer", "Error", JOptionPane.ERROR_MESSAGE);
           }
