de6727488e3a2a2a6d8c304348384e426c1b2084
Yiteng <31704724+GeekyNerds@users.noreply.github.com>
2017-12-12 21:30:16 -0800

    update getAllVehiclesMap() methods

--- src/com/neuSep17/service/InventoryServiceAPI_Test.java
+++ src/com/neuSep17/service/InventoryServiceAPI_Test.java
@@ -66,7 +66,7 @@ public class InventoryServiceAPI_Test {
 		File[] files = dir.listFiles();
 		
 		for(File file : files) {
-			if(file!= null && file.isFile() && file.getName().endsWith(".txt")) {
+			if(file!= null && file.isFile() && file.getName().endsWith(".txt") && file.getName().startsWith("gmps")) {
 				BufferedReader reader = new BufferedReader(new FileReader(file));
 				String line = null;
 				int count = 0;
