87d015ef75bc67dfe6a41939f8c3ee6e6dede042
YujiaNEU2017 <zhang.yujia@husky.neu.edu>
2017-12-13 17:21:17 -0800

    update browse inventory size

--- src/com/neuSep17/ui/consumer/BrowseInventoryFrame.java
+++ src/com/neuSep17/ui/consumer/BrowseInventoryFrame.java
@@ -282,7 +282,6 @@ public class BrowseInventoryFrame extends JFrame implements Runnable
 
     private void makeThisVisible()
     {
-        this.setSize(1920, 1200);
         this.setMinimumSize(new Dimension(900, 600));
         this.setVisible(true);
     }
